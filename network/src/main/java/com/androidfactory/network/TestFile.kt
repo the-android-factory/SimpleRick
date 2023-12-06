c59a930bbfbaf33c396193269034bf291b22cd1b
Dom <droidfactory95@gmail.com>
2023-12-05 22:21:17 -0500

    Removed unused file

--- network/src/main/java/com/androidfactory/network/TestFile.kt
+++
@@ -1,9 +0,0 @@
-package com.androidfactory.network
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TestFile() {
-    Text(text = "Hello from the other module")
-}
