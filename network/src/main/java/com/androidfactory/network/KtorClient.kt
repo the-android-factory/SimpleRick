1de2eace392b8dd4216a768e4a7b4f7f6e3eafe7
Dom <droidfactory95@gmail.com>
2023-12-19 00:03:26 -0500

    Formatting

--- network/src/main/java/com/androidfactory/network/KtorClient.kt
+++ network/src/main/java/com/androidfactory/network/KtorClient.kt
@@ -52,8 +52,8 @@ class KtorClient {
 }
 
 sealed interface ApiOperation<T> {
-    data class Success<T>(val data: T): ApiOperation<T>
-    data class Failure<T>(val exception: Exception): ApiOperation<T>
+    data class Success<T>(val data: T) : ApiOperation<T>
+    data class Failure<T>(val exception: Exception) : ApiOperation<T>
 
     fun onSuccess(block: (T) -> Unit): ApiOperation<T> {
         if (this is Success) block(data)
