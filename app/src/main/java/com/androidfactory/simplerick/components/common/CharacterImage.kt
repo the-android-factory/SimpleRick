10a2e09c1b47af59afc4c927b22e4d4525fd3bbf
Dom <droidfactory95@gmail.com>
2024-03-24 16:16:15 -0400

    CharacterImage composable updates

--- app/src/main/java/com/androidfactory/simplerick/components/common/CharacterImage.kt
+++ app/src/main/java/com/androidfactory/simplerick/components/common/CharacterImage.kt
@@ -1,11 +1,15 @@
 package com.androidfactory.simplerick.components.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
 
@@ -21,5 +25,18 @@ fun CharacterImage(imageUrl: String, modifier: Modifier = defaultModifier) {
         contentDescription = "Character image",
         modifier = modifier,
         loading = { LoadingState() }
+    )
+}
+
+@Preview
+@Composable
+private fun CharacterImagePreview() {
+    CharacterImage(
+        imageUrl = "image url",
+        modifier = defaultModifier.then(
+            Modifier.background(
+                brush = Brush.verticalGradient(listOf(Color.White, Color.Black))
+            )
+        )
     )
 }
